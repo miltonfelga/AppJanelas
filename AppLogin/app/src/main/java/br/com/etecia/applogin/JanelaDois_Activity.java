087eb0ac6f921460d5cfdca9ae42d297bfd2fbbc
milton felga <milton.felga@etec.sp.gov.br>
2019-09-18 19:30:23 -0300

    Layout azul pronto

---
+++ AppLogin/app/src/main/java/br/com/etecia/applogin/JanelaDois_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.applogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JanelaDois_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_janela_dois);
+    }
+}
