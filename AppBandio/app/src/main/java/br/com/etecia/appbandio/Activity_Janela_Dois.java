0b8a357462aead9f7b0145a56015c67509917a07
manager <kaduce2009@hotmail.com>
2019-10-02 22:10:16 -0300

    adicionado barra puxavel e segundo layout bandio

---
+++ AppBandio/app/src/main/java/br/com/etecia/appbandio/Activity_Janela_Dois.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appbandio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Janela_Dois extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janela_dois_layout);
+    }
+}
---
+++ MyToolBarScroll-master/app/src/main/java/br/com/local/mytoolbarscroll/MainActivity.java
@@ -0,0 +1,26 @@
+package br.com.local.mytoolbarscroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.idToolBar);
+        setSupportActionBar(toolbar);
+
+    }
+}
