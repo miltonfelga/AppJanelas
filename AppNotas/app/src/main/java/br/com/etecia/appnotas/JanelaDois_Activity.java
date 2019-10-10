313a8b8d62c5e80fb73d19e3d647c0054dafacfa
christian <christian.nascimento3@etec.sp.gov.br>
2019-10-09 22:11:05 -0300

    finalizando appnotas e appprofile

---
+++ AppNotas/app/src/main/java/br/com/etecia/appnotas/JanelaDois_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appnotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JanelaDois_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janela_dois_layout);
+    }
+}
