d2715e0e5ad04e36f5a1ee4e9bea3d4102c5422b
christian <christian.nascimento3@etec.sp.gov.br>
2019-11-06 20:17:03 -0300

    adicionando loading no app estrela e iniciando app musica

---
+++ appEstrela/app/src/main/java/br/com/etecia/appestrela/JanelaTres_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appestrela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JanelaTres_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janela_tres_layout);
+    }
+}
